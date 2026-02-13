a9fff5cd7ba3ccd763a3ec2263b2e17e2946d3c8
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2026-02-13 22:47:28 +0300

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -23,8 +23,17 @@ builder.Services
     .AddApplicationRepositories()
     .AddApplicationServices();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+app.UseCors(builder =>
+{
+    builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200");
+});
 app.MapControllers();
 
 app.Run();
