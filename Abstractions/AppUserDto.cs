009698ff87431c5dd3a70190c4c935a74c7f04e8
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2026-05-18 22:30:29 +0300

    Cleared the project.

--- Abstractions/AppUserDto.cs
+++
@@ -1,7 +0,0 @@
-namespace Abstractions;
-
-public class AppUserDto
-{
-    public int Id { get; set; }
-    public string? UserName { get; set; }
-}
--- Entities/AppUser.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Entities;
-
-public class AppUser
-{
-    public int Id { get; set; }
-    public required string UserName { get; set; }
-}
